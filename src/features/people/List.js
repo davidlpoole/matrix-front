c71a5e9cf1ef033dd0ee15c3894a334e1ad5ce7a
David Poole <14233215+davidlpoole@users.noreply.github.com>
2021-10-05 22:03:26 +1300

    basic form to add new person

--- src/features/people/List.js
+++ src/features/people/List.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 
 import { saveNewPerson, selectPeopleIds } from './peopleSlice';
@@ -6,21 +6,67 @@ import ListItem from './ListItem';
 
 export function List() {
 
-
   const dispatch = useDispatch()
+
 
   const people = useSelector(selectPeopleIds)
 
-  const testPerson = {
-    name: 'Mike',
-    position: 'DC',
-    site: 'MTW',
-    department: 'Warehouse',
-    shift: 'AM',
+  // const testPerson = {
+  //   name: 'Mike',
+  //   position: 'DC',
+  //   site: 'MTW',
+  //   department: 'Warehouse',
+  //   shift: 'AM',
+  // }
+
+
+
+  const useField = (type) => {
+    const [value, setValue] = useState('')
+    const onChange = (event) => {
+      setValue(event.target.value)
+    }
+
+    return {
+      type,
+      value,
+      onChange
+    }
   }
 
-  const onAdd = () => {
-    dispatch(saveNewPerson(testPerson))
+  const AddPersonForm = () => {
+
+    const name = useField('text')
+    const site = useField('text')
+    const department = useField('text')
+    const shift = useField('text')
+    const position = useField('text')
+
+    const newPerson = {
+      name: name.value,
+      site: site.value,
+      department: department.value,
+      shift: shift.value,
+      position: position.value
+    }
+
+    const onAdd = (event) => {
+      event.preventDefault()
+      dispatch(saveNewPerson(newPerson))
+    }
+
+    return (
+      <div>
+        <form onSubmit={onAdd}>
+          <div><input {...name} placeholder='name' /></div>
+          <div><input {...site} placeholder='site' /></div>
+          <div><input {...department} placeholder='department' /></div>
+          <div><input {...shift} placeholder='shift' /></div>
+          <div><input {...position} placeholder='position' /></div>
+          <button type='submit'>addTest</button>
+        </form>
+      </div>
+    )
   }
 
   const renderedListItems = people.map((person) => {
@@ -29,7 +75,9 @@ export function List() {
 
   return (
     <div>
-      <button onClick={onAdd}>addTest</button>
+      <div>
+        <AddPersonForm />
+      </div>
       <ul>
         {renderedListItems}
       </ul>
