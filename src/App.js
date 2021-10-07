847ef01f19e9aaf53c4d9b6505dd80a6aebbbe40
David Poole <14233215+davidlpoole@users.noreply.github.com>
2021-10-07 19:13:07 +1300

    testing out dark mode

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import { List } from './features/people/List';
 import './App.css';
+import './styles.scss';
+import useDarkMode from './features/hooks/use-dark-mode'
+import Toggle from './features/generic/Toggle';
 
 function App() {
+  const [darkMode, setDarkMode] = useDarkMode();
 
   return (
     <div className="App">
+      <div className="navbar">
+        <Toggle darkMode={darkMode} setDarkMode={setDarkMode} />
+      </div>
       <List />
     </div>
   );
